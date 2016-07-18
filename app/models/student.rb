36cd1533137ca6ffbcae462403c98fdd4861b468
Vinicius Venancio <vinicius.venancio@live.com>
2016-07-18 15:27:23 -0300

    feature: implements student validations

---
+++ app/models/student.rb
@@ -0,0 +1,5 @@
+class Student < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 45 }
+  validates :register_number, presence: true, length: { maximum: 45 }
+  validates :status, presence: true, numericality: { only_integer: true }
+end
---
+++ spec/models/student_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Student, type: :model do
+  context 'with validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_length_of(:name).is_at_most(45) }
+    it { is_expected.to validate_presence_of(:register_number) }
+    it { is_expected.to validate_length_of(:register_number).is_at_most(45) }
+    it { is_expected.to validate_presence_of(:status) }
+    it { is_expected.to validate_numericality_of(:status).only_integer }
+  end
+end
